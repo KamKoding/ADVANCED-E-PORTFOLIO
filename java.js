7a1cdd0eca532273dc4a7cf2855b4b622bc607a4
KamKoding <qkamarie@gmail.com>
2026-05-11 23:24:11 -0700

    ModalPostJS

---
+++ java.js
@@ -0,0 +1,27 @@
+// template_h5xk6ft
+// service_19mqubv
+// SxZAkz0gnMLtcL-GL
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList.add("modal__overlay--visible");
+  emailjs
+    .sendForm(
+      "service_19mqubv",
+      "template_h5xk6ft",
+      event.target,
+      "SxZAkz0gnMLtcL-GL",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList.add("modal__overlay--visible");
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please email me directly @qkamarie@gmail.com ",
+      );
+    });
+}
